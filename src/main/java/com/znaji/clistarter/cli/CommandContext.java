b5ccfb033b18d78c18faa6274efd139f9450492c
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-06-08 12:00:39 +0100

    feature: ArgsParser: - takes the console input as string and parse it - returns the CommandContext(has a list of raw args, and a map of named args)

---
+++ src/main/java/com/znaji/clistarter/cli/CommandContext.java
@@ -0,0 +1,30 @@
+package com.znaji.clistarter.cli;
+
+import java.util.List;
+import java.util.Map;
+
+public class CommandContext {
+    private final List<String> rawArgs;
+    private final Map<String, String> namedArgs;
+
+    public CommandContext(List<String> rawArgs, Map<String, String> namedArgs) {
+        this.rawArgs = rawArgs;
+        this.namedArgs = namedArgs;
+    }
+
+    public List<String> getArgs() {
+        return rawArgs;
+    }
+
+    public Map<String, String> getNamedArgs() {
+        return namedArgs;
+    }
+
+    public String get(String name) {
+        return namedArgs.get(name);
+    }
+
+    public boolean has(String name) {
+        return namedArgs.containsKey(name);
+    }
+}
